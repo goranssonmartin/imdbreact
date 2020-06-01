88c9c8d2bf28eb89100fbf70003aaff200f3f600
Martin <martin.goransson94@gmail.com>
2020-06-01 17:25:31 +0200

    Added props to card

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,13 +1,13 @@
 import React, {Component} from "react";
 import "../Styling/Card.css";
-import "./Button";
+import Button from "./Button";
 class Card extends Component{
 render() {
   return (<tr>
-  <td><img></img></td>
-  <td><p></p></td>
-  <td><p></p></td>
-  <button></button>
+  <td><img src={this.props.imageSource} alt={this.props.name}></img></td>
+  <td><p>{this.props.name}</p></td>
+  <td><p>{this.props.popularity}</p></td>
+  <Button/>
   </tr>);
 }}
 export default Card;
